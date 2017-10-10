0ab55d8a84e59aa3d064d43e8ad59cb2ff7428b9
uuinfo-developer3 <apple3@ioioinfo.com>
2017-10-10 16:06:06 +0800

    all

--- app/statistics.jsx
+++ app/statistics.jsx
@@ -7,6 +7,93 @@ var Left = require('Left');
 var WrapBottom = require('WrapBottom');
 var ChangePassword = require('ChangePassword');
 
+
+import { Provider, connect } from 'react-redux'
+import { createStore } from 'redux'
+
+function statistics(state, action) {
+  switch (action.type) {
+  case 'STATISTIC_DETAIL':
+    {
+        var date1 = action.date1;
+        if (!date1) {
+            date1 = "";
+        }
+        var store_id = state.store_id;
+        if (!store_id) {
+            store_id = "";
+        }
+
+      $.ajax({
+         url: '/get_orders_byDate?date='+date1+"&store_id=" + store_id,
+         dataType: 'json',
+         type: 'GET',
+         success: function(data) {
+           if (data.success) {
+             store.dispatch({ type: 'GET_DATA', data: data});
+           }else {
+           }
+         }.bind(this),
+         error: function(xhr, status, err) {
+         }.bind(this)
+      });
+
+      return state;
+    }
+  case 'GET_DATA':
+  {
+    var data = action.data;
+    var mendian_items = state.mendian_items;
+    return {item:data,pay_map:data.pay_map,pay_ways:data.pay_ways,mendian_items:mendian_items,store_id:state.store_id,date1:state.date1};
+  }
+  case 'MENDIAN_DETAIL':
+    {
+      $.ajax({
+         url: '/store_list',
+         dataType: 'json',
+         type: 'GET',
+         success: function(data) {
+           if (data.success) {
+             store.dispatch({ type: 'GET_DATA1', mendian_data: data});
+           }else {
+           }
+         }.bind(this),
+         error: function(xhr, status, err) {
+         }.bind(this)
+      });
+
+      return state;
+    }
+  case 'GET_DATA1':
+  {
+    var mendian_data = action.mendian_data;
+    return {item:state.item, pay_map:state.pay_map, pay_ways:state.pay_ways, mendian_items:mendian_data.rows,store_id:state.store_id,date1:state.date1};
+  }
+case 'SEARCH_DETAIL':
+    {
+        var store_id = action.store_id;
+        return {item:state.item, pay_map:state.pay_map, pay_ways:state.pay_ways, mendian_items:state.mendian_items,store_id:store_id,date1:state.date1};
+    }
+  default:
+    return state
+  }
+}
+
+let store = createStore(statistics,{item:{},pay_map:{},pay_ways:[],mendian_items:[],store_id:"",date1:""});
+
+const mapStateToProps = (state) => {
+    return {
+        item: state.item,
+        pay_map: state.pay_map,
+        pay_ways: state.pay_ways,
+        mendian_items: state.mendian_items,
+        store_id: state.store_id,
+        date1: state.date1,
+
+    }
+}
+
+
 // 框架
 class Wrap extends React.Component {
     render() {
@@ -41,43 +128,71 @@ class Right extends React.Component {
 };
 
 
-class Middle extends React.Component {
+class MiddleClass extends React.Component {
     constructor(props) {
         super(props);
-        // 初始化一个空对象
-        this.state = {items: {}};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     componentDidMount() {
-        $.ajax({
-          url: "/get_orders_byDate",
-          dataType: 'json',
-          type: 'GET',
-          success: function(data) {
-              this.setState({items:data});
-          }.bind(this),
-          error: function(xhr, status, err) {
-          }.bind(this)
-      });
+        store.dispatch({ type: 'MENDIAN_DETAIL'});
+    }
+    handleChange(id){
+        $('.mendian_infor').removeClass('mendian_back');
+        $('.mendian_infor'+id).addClass('mendian_back');
+
+        store.dispatch({ type: 'SEARCH_DETAIL',store_id:id});
+    }
+    handleClick(e){
+        var date1 = $('#time').val();
+
+        store.dispatch({ type: 'STATISTIC_DETAIL', date1:date1});
     }
     render() {
-        var time = this.state.items.time;
-        var total_sales = this.state.items.total_sales;
-        var order_num = this.state.items.order_num;
-        var total_products = this.state.items.total_products;
+        var time = this.props.item.time;
+        var total_sales = this.props.item.total_sales;
+        var order_num = this.props.item.order_num;
+        var total_products = this.props.item.total_products;
         return (
             <div className="statistics_middle">
                 <p className="back">统计时间:{time}</p>
-                <div className="col-xs-6 col-sm-6 number number1"><p>￥{total_sales}</p><p>营业额</p></div>
-                <div className="col-xs-6 col-sm-6 number number2"><p>{order_num}</p><p>订单数</p></div>
-                <div className="col-xs-6 col-sm-6 number number3"><p>{total_products}</p><p>件数</p></div>
-                <div className="col-xs-6 col-sm-6 number number4"><p>暂无</p></div>
+                <div className="col-sm-12 col-md-6 statistics_middle_left_border">
+                    <h3>选择门店</h3>
+                    <div className="meidian_wrap">
+                        {this.props.mendian_items.map((item,index )=> (
+                            <div className="mendian_list col-sm-6 col-md-4 " key={index}>
+                                <div className={"mendian_infor  mendian_infor"+item.org_store_id} onClick={this.handleChange.bind(this,item.org_store_id)}>{item.org_store_name}</div>
+                            </div>
+                        ))
+                        }
+
+                    </div>
+
+                    <h3 className="margin_top"> 时间查询</h3>
+                    <div className="input-group">
+                        <input type="text" className="form-control" id="time" placeholder="输入查询如期例如 ‘2017-01-01’"/>
+                        <span className="input-group-addon" onClick={this.handleClick}>查询</span>
+                    </div>
+                </div>
+                <div className="col-sm-12 col-md-6 statistics_middle_right">
+                    <div className="col-xs-6 col-sm-6 number"><p>￥{total_sales}</p><p>营业额</p></div>
+                    <div className="col-xs-6 col-sm-6 number"><p>{order_num}</p><p>订单数</p></div>
+                    <div className="col-xs-6 col-sm-6 number"><p>{total_products}</p><p>件数</p></div>
+                    {this.props.pay_ways.map((item,index )=> (
+                        <div className="col-xs-6 col-sm-6 number" key={index}><p>￥{this.props.pay_map[item]}</p><p>{item}</p></div>
+                    ))
+                    }
+                </div>
             </div>
         );
     }
 };
+const Middle = connect(mapStateToProps)(MiddleClass);
 // 返回到页面
 ReactDOM.render(
-    <Wrap/>,
+    <Provider store={store}>
+    <Wrap/>
+    </Provider>,
     document.getElementById("content")
 );
